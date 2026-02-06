5508a70800b43144a0d019460947356f564eed32
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2026-02-05 23:11:10 -0500

    Changed login page color schema around and began work on a frontpage

--- conami/src/app/page.js
+++ conami/src/app/page.js
@@ -1,11 +1,14 @@
 import Homepage from './Home/page.js';
-import Sidebar from './Components/sidebar.js'
-import Login from './Login/page.js'
+import Sidebar from './Components/sidebar.js';
+import Login from './Login/page.js';
+import FrontPage from './FrontPage';
 
 export default function MainApp() {
   return (
-    <Login/>
-      /* // <Homepage/> */
+    <>
+      <FrontPage />
+      <Login />
+    </>
   );
 }
 
