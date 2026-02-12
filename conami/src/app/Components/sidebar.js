b149a4a5d6c1490175c38d9af3aa433cfe7d7192
robotsofi <sofia.k.pardo@gmail.com>
2026-02-11 20:36:55 -0500

    Matched Signup to Login design; minimal changes to chatroom

--- conami/src/app/Components/sidebar.js
+++ conami/src/app/Components/sidebar.js
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import { FiUser, FiMessageSquare, FiSettings } from "react-icons/fi";
+import Logo from "./Logo";
 
 export default function Sidebar() {
     return (
@@ -8,19 +9,22 @@ export default function Sidebar() {
             style={{ backgroundColor: "#8D4F3F" }}
         >
             <nav className="p-4 space-y-2 text-white">
-
+                <Logo/>
                 <Link
                     href="/Profile"
                     className="flex items-center gap-2 text-[#C37050] hover:text-white"
                 >
                     <FiUser size={24} />
-                    <span className="underline cursor-pointer">Profile</span>
+                    <span className="cursor-pointer">Profile</span>
                 </Link>
 
-                <a className="flex items-center gap-2 text-[#C37050] hover:text-white cursor-pointer">
+                <Link
+                    href="/ChatRoom"
+                    className="flex items-center gap-2 text-[#C37050] hover:text-white"
+                    >
                     <FiMessageSquare size={24} />
                     Chats
-                </a>
+                </Link>
 
                 <a className="flex items-center gap-2 text-[#C37050] hover:text-white cursor-pointer">
                     <FiSettings size={24} />
--- conami/src/app/page.js
+++ conami/src/app/page.js
@@ -7,7 +7,7 @@ export default function MainApp() {
   return (
     <>
       <FrontPage />
-      <Login />
+      {/* <Login /> */}
     </>
   );
 }
