93738c9bf0264532457ce7dec3a2c3246c17aed4
Fabian Garcia <fgar877@gmail.com>
2026-03-06 23:14:43 -0500

    Added conversation, messages api routes

--- conami/backend/main.py
+++ conami/backend/main.py
@@ -26,13 +26,13 @@ class User(SQLModel, table=True):
     password: str = Field(index=True)
 
 class Conversations(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     user1: str = Field(foreign_key="user.username")
     user2: str = Field(foreign_key="user.username")
     created_at: datetime = Field(default_factory=datetime.utcnow)
 
 class Message(SQLModel, table=True):
-    id: int = Field(primary_key=True)
+    id: int | None = Field(default=None, primary_key=True)
     conversation_id: int = Field(foreign_key="conversations.id")
     sender_username: str = Field(foreign_key="user.username")
     content: str = Field()
@@ -61,13 +61,20 @@ def create_user(user: User ,session: SessionDep) -> User:
         session.rollback()
         raise HTTPException(status_code=400, detail="Username already exists")
 
+
+@app.get("/users/")
+def get_users(session: SessionDep) -> list[User]:
+    users = session.exec(select(User)).all()
+    return users
+
+
 # LOGIN ENDPOINT
 @app.post("/auth/login")
 def login(
     session: SessionDep,
     username: str = Body(...),
     password: str = Body(...)
-):
+    ):
     user = session.get(User, username)
 
     if not user or user.password != password:
@@ -75,12 +82,47 @@ def login(
 
     return {"message": "Login successful"}
 
+# Create Conversations
+@app.post("/conversations")
+def create_conversation(
+    session: SessionDep, 
+    user1: str = Body(...), 
+    user2: str = Body(...)
+    ):
 
-@app.get("/users/")
-def get_users(session: SessionDep) -> list[User]:
-    users = session.exec(select(User)).all()
-    return users
+    new_conversation = Conversations(user1=user1, user2=user2)
+    session.add(new_conversation)
+    session.commit()
+    session.refresh(new_conversation)
+    return new_conversation
 
-# CHAT ENDPOINT
-@app.post("/chat")
-def get_chat
+# GET ALL CONVERSATIONS FOR A USER
+@app.get("/conversations")
+def get_conversations(session: SessionDep, username: str = Query(...)):
+    conversations = session.exec(select(Conversations).where(
+        (Conversations.user1 ==username) | (Conversations.user2 == username)
+        )
+    ).all()
+    return conversations
+
+# GET ALL MESSAGES IN A CONVERSATION
+@app.get("/conversations/{conversation_id}/messages")
+def get_messages(conversation_id : int, session: SessionDep):
+    messages = session.exec(select(Message).where(Message.conversation_id == conversation_id)
+    ).all()
+    return messages
+
+#SEND A MESSAGE
+@app.post("/conversations/{conversation_id}/messages")
+def send_message(
+    conversation_id : int, 
+    session: SessionDep,
+    sender_username: str= Body(...),
+    content: str = Body(...)
+    ):
+    new_message = Message(conversation_id=conversation_id, sender_username=sender_username, content=content)
+    session.add(new_message)
+    session.commit()
+    session.refresh(new_message)
+    return new_message
+    # we put the conversation id, sender-username and content all in the message table
