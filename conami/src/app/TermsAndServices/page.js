51fc73fc193cf57e21d34e6954aaf0eb68397de9
robotsofi <kayzlee08@gmail.com>
2026-03-08 01:46:03 -0500

    Created the terms and services page

---
+++ conami/src/app/TermsAndServices/page.js
@@ -0,0 +1,20 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+
+export default function TermsAndServices() {
+   
+};
+
+//after a user signs up, they will be redirected to this page. They must click the "I agree" box before creating an account
+
+return (
+    <div className="flex items-center justify-center min-h-screen bg-[#f0e1d1]">
+        <div className="flex w-full max-w-md shadow-2xl rounded-sm flex-col items-center p-10 bg-white">
+            <h1 className="text-lg font-medium">TERMS AND SERVICES</h1>
+            <h2 >PLEASE ENSURE YOU HAVE READ AND UNDERSTOOD BEFORE PROCEEDING. YOU MUST AGREE TO THE TERMS AND SERVICS TO CREATE AN ACCOUNT.</h2>
+            <p>Our application requires users to be a minimum age of 18 years old.</p>
+        </div>
+    </div>
+
+)
