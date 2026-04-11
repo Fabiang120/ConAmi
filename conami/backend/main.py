3f19e3b38887c247e63062a93105f5a2e353bcb7
Fabiang120 <117304307+Fabiang120@users.noreply.github.com>
2026-04-11 16:55:01 -0400

    Added more stuff

--- conami/backend/main.py
+++ conami/backend/main.py
@@ -176,21 +176,19 @@ def set_auth_cookie(response: Response, username: str) -> None:
 # SignUp API. route
 # SignUp API. route
 @app.post("/users/")
-def create_user(user: User, session: SessionDep, response: Response) -> dict:
+def create_user(user: User, session: SessionDep, response: Response):
     check_regexes(user.username, user.password)
     user.password = pwd_context.hash(user.password)
     session.add(user)
+    session.commit()  
+    session.refresh(user)
+    
     new_profile = Profile(username=user.username)
     session.add(new_profile)
+    session.commit()  
     
-    try:
-        session.commit()
-        session.refresh(user)
-        set_auth_cookie(response, user.username)
-        return {"ok": True}
-    except IntegrityError:
-        session.rollback()
-        raise HTTPException(status_code=400, detail="Username already exists")
+    set_auth_cookie(response, user.username)
+    return {"ok": True}
 
 # This function decodes JWT, extracts username from sub, and returns full user object 
 @app.get("/auth/me")
